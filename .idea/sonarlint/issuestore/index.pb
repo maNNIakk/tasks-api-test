340a25fc2274915c241df220710087eb4ec696b5
Renato Santos <renato.santos@reserve.com.br>
2024-03-03 23:44:32 -0300

    incluido teste de api para contemplar remoção de task

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,6 @@
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
 g
 7src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java,e\4\e4148d8791ec1608409d9b55b60f8afd6dfee019
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
--- src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -52,4 +52,25 @@ public class APITest {
                 .body("message", CoreMatchers.is("Due date must not be in past"));
 
     }
+
+    @Test
+    public void deveRemoverTarefaComSucesso() {
+        Integer id = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body("{ \"task\": \"Teste para deletar via API\",\n" +
+                        "        \"dueDate\": \"2025-12-30\"\n" +
+                        "    \n" +
+                        "        }\n")
+                .when()
+                .post("/todo")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract().path("id");
+        RestAssured.given()
+                .when()
+                .delete("/todo/" +id)
+                .then()
+                .statusCode(204);
+    }
 }
